d613d15e2309116e02c4af945a28432bf4d07d1c
ingryong <bbgod2@gmail.com>
2019-12-14 01:48:30 +0900

    메인페이지
    상단바 home 추가
    home부분 이미지 추가
    타이핑 애니메이션 추가 및 불투명 배경 추가

--- assets/js/index.js
+++ assets/js/index.js
@@ -71,9 +71,30 @@ $(function(){
 		evt.stopPropagation();
 	});
 	
+
+	//글자 타이핑 효과
+	var typingBool = false; 
+    var typingIdx=0; 
+    var typingTxt = $(".typing-txt").text(); // 타이핑될 텍스트를 가져온다 
+    typingTxt=typingTxt.split(""); // 한글자씩 자른다. 
+    if(typingBool==false){ // 타이핑이 진행되지 않았다면 
+       typingBool=true; 
+       
+       var tyInt = setInterval(typing,100); // 반복동작 
+     } 
+     
+     function typing(){ 
+       if(typingIdx<typingTxt.length){ // 타이핑될 텍스트 길이만큼 반복 
+         $(".typing").append(typingTxt[typingIdx]); // 한글자씩 이어준다. 
+         typingIdx++; 
+       } else{ 
+         clearInterval(tyInt); //끝나면 반복종료 
+       } 
+     }  
+
+
 	
-	
-	console.log('새로운 홈페이지를 준비 중이에요! :D');
+	console.log('새로운 홈페이지를 준비 중이에요!');
     
 	//윈도우 로드 이벤트
 	$(window).on("load resize",function(){
